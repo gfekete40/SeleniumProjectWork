feb19d56b531c73a05a6526a8f5678ea34546f42
Fekete Gábor <gabor.fekete30@gmail.com>
2024-01-11 20:18:57 +0100

    01.11.

---
+++ src/test/java/hu/masterfield/pages/TransactionsPage.java
@@ -0,0 +1,11 @@
+package hu.masterfield.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class TransactionsPage extends BasePage {
+
+    public TransactionsPage(WebDriver driver) {
+        super(driver);
+    }
+}
